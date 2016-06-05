3a0f3ac37d1cab9a570a16303dfd58c264112fe8
LinEmsber <LinEmsber@gmail.com>
2016-06-05 21:40:52 +0800

    pthread

--- threads/pthread_mutex_01.c
+++ threads/pthread_mutex_01.c
@@ -1,97 +1,67 @@
+/* A simple implementation of pthread_mutex  */
+
 /*
- * This is a simple serial program which computes the dot product of two
- * vectors.  The threaded version can is dotprod_mutex.c.
+ * Add and subtraction of the global variable avail in multi-threads,
+ * and using mutex to make sure that the race condition would not
+ * happen.
  */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
-#define VEC_LEN 1000
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int avail = 0;
 
-typedef struct _dot dot;
+void *fun(void *arg)
+{
+	int i;
+	int counter = atoi( arg);
 
-struct _dot{
-	double *a;
-	double *b;
-	double sum;
-	int vec_len;
-};
+	for ( i=0; i< counter; i++){
+		// sleep(1);
 
-
-dot dotstr;
-pthread_t call_threads[NUM_THREADS];
-pthread_mutex_t mutex;
-
-/* Define and use local variables for convenience */
-void dot_product() {
-
-	int start, end, i;
-	double mysum, *x, *y;
-
-	start = 0;
-	end = dotstr.vec_len;
-	x = dotstr.a;
-	y = dotstr.b;
-
-	mysum = 0;
-	for (i=start; i<end ; i++)	{
-		mysum += (x[i] * y[i]);
+		pthread_mutex_lock(&mutex);
+		printf("%d\n", i);
+		avail++;
+		pthread_mutex_unlock(&mutex);
 	}
-
-	pthread_mutex_lock (&mutex);
-	dotstr.sum += mysum;
-	pthread_mutex_unlock (&mutex);
-
-	pthread_exit((void*) 0);
+	return NULL;
 }
 
-
-int main (int argc, char *argv[])
+int main( int argc, char *argv[])
 {
 
-	int i;				// Iteration
-	int len = VEC_LEN;
-	double *a, *b;
-	void *status;
-	pthread_attr_t attr;
+	int i;
+	pthread_t tid;
+	int total_requested;
+	int unit;
+	int token;
 
-	// Assign storage and initialize values
-	a = (double*) malloc (NUM_THREADS*VEC_LEN*sizeof(double));
-	b = (double*) malloc (NUM_THREADS*VEC_LEN*sizeof(double));
-
-	for (i=0; i<len; i++) {
-		a[i]=1;
-		b[i]=a[i];
+	// adding of natural number
+	total_requested = 0;
+	for (i=0; i<argc; i++){
+		total_requested = total_requested + atoi(argv[i]);
+		pthread_create(&tid, NULL, fun, argv[i]);
 	}
 
-	dotstr.vec_len = len;
-	dotstr.a = a;
-	dotstr.b = b;
-	dotstr.sum = 0;
+	unit = 0;
+	token = 0;
+	for (;;){
+		pthread_mutex_lock(&mutex);
 
-	pthread_mutex_init(&mutex, NULL);
+		while( avail>0 ){
+			unit++;
+			avail--;
+			printf("unit=%d\n", unit);
+			token = (unit >= total_requested)? 1:0;
+		}
 
-	// Create threads to perform the dotproduct
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+		pthread_mutex_unlock(&mutex);
 
-	for(i=0; i<NUM_THREADS; i++)	{
-		pthread_create(&call_threads[i], &attr, dot_product, (void *)i);
+		if(token)
+			break;
 	}
-
-	pthread_attr_destroy(&attr);
-
-	// Wait on the other threads
-	for(i=0; i<NUM_THREADS; i++)	{
-		pthread_join(call_threads[i], &status);
-	}
-
-	// After joining, print out the results and cleanup
-	printf ("Sum =  %f \n", dotstr.sum);
-
-	free (a);
-	free (b);
-	pthread_mutex_destroy(&mutex);
-	pthread_exit(NULL);
+	return 0;
 }
