a34f8aede1b855fcd3f5dc508138bc29ed37d0e9
LinEmsber <LinEmsber@gmail.com>
2016-08-10 22:26:04 +0800

    file location seek

---
+++ file_IO/lseek_01.c
@@ -0,0 +1,33 @@
+/* An example of lseek() */
+
+// The off_t data type is a signed integer data type specified by POSIX.1.
+// grep -r off_t /usr/include/sys/types.h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+	// SEEK_SET
+	off_t ret;
+	ret = lseek(fd, (off_t) 1825, SEEK_SET);
+	if (ret == (off_t) -1){
+		perror("lseek");
+	}
+
+	// SEEK_END
+	off_t ret;
+	ret = lseek(fd, 0, SEEK_END);
+	if (ret == (off_t) -1){
+		perror("lseek");
+	}
+
+	// SEEK_CUR
+	off_t ret;
+	ret = lseek(fd, 0, SEEK_CUR);
+	if (ret == (off_t) -1){
+		perror("lseek");
+	}
+
+}
