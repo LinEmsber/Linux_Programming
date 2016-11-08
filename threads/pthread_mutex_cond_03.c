43bc692129432f1867b96609393e46d154d98886
LinEmsber <LinEmsber@gmail.com>
2016-11-08 11:21:14 +0800

    re-organization and add more comment

--- threads/pthread_mutex_cond_03.c
+++ threads/pthread_mutex_cond_03.c
@@ -12,10 +12,20 @@
 #include <pthread.h>
 
 #define CAPACITY 6
-int buffer[CAPACITY];
-int in;
-int out;
+#define ITEM_COUNT (CAPACITY * 2)
 
+
+// ============ global variable ==============
+int buffer[CAPACITY];
+int in = 0;
+int out = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t wait_empty_buffer;
+pthread_cond_t wait_full_buffer;
+
+
+// ============ item operantion function ==============
 int buffer_is_empty()
 {
         return in == out;
@@ -37,6 +47,7 @@ int get_item()
 
 void put_item(int item)
 {
+        // save item into buffer at index, in.
         buffer[in] = item;
 
         // in++;
@@ -45,12 +56,7 @@ void put_item(int item)
         in = (in + 1) % CAPACITY;
 }
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t wait_empty_buffer;
-pthread_cond_t wait_full_buffer;
-
-#define ITEM_COUNT (CAPACITY * 2)
-
+// ============ consumer() and producer() ==============
 void *consumer(void *arg)
 {
         int i;
@@ -59,16 +65,18 @@ void *consumer(void *arg)
         for (i = 0; i < ITEM_COUNT; i++) {
                 pthread_mutex_lock(&mutex);
 
-                // wait wait_full_buffer from producer
+                // wait the pthread condition variable, wait_full_buffer, from producer
                 while (buffer_is_empty())
                         pthread_cond_wait(&wait_full_buffer, &mutex);
 
+                // get the item from global array
                 item = get_item();
 
-                // The pthread_cond_signal() function unblocks one thread waiting for the
-                // condition variable cond.
+                /*
+                 * The pthread_cond_signal() function unblocks one thread waiting for the condition variable cond.
+                 */
 
-                // wake up wait_full_buffer in the conumer function
+                // After get item from buffer, wake up the pthread condition variable, wait_full_buffer, in the conumer function
                 pthread_cond_signal(&wait_empty_buffer);
 
                 pthread_mutex_unlock(&mutex);
@@ -86,20 +94,21 @@ void producer()
 
         for (i = 0; i < ITEM_COUNT; i++) {
 
-                // print char starts from a, and wait 0.5 secs
+                // assign and print a char to item, and wait 0.5 secs
                 item = i + 'a';
                 printf("produce : %c\n", item);
                 usleep(500000);
 
                 pthread_mutex_lock(&mutex);
 
-                // wait wait_empty_buffer from conumer
+                // wait the pthread condition variable, wait_empty_buffer, from conumer
                 while (buffer_is_full())
                         pthread_cond_wait(&wait_empty_buffer, &mutex);
 
+                // save the item into global array, buffer, and update the index of this array
                 put_item(item);
 
-                // wake up wait_full_buffer in the conumer function
+                // After put_item(), wake up the pthread condition variable, wait_full_buffer, in the conumer function
                 pthread_cond_signal(&wait_full_buffer);
 
                 pthread_mutex_unlock(&mutex);
@@ -120,10 +129,10 @@ int main()
         pthread_cond_init(&wait_empty_buffer, NULL);
         pthread_cond_init(&wait_full_buffer, NULL);
 
-        // create a thread for consumer, but this consumer wait for producer
+        // create a thread for consumer(), but this consumer() will wait for producer().
         pthread_create(&consumer_tid, NULL, consumer, NULL);
 
-        // producer output char and make condition variable for consumer who is waiting.
+        // producer() output char and send the condition variable, wait_full_buffer, for consumer() who is waiting.
         producer();
 
         return 0;
