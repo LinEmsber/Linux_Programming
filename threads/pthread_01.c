18036dddad98d52062020104982aba94f8f1f104
Lin Emsber <linemsber@gmail.com>
2017-04-14 23:44:46 +0800

    Review pthread
    1. pthread_create()
    2. pthread_join()

--- threads/pthread_01.c
+++ threads/pthread_01.c
@@ -5,52 +5,49 @@
 #include <errno.h>
 #include <pthread.h>
 
-// this function is run by the second thread
-void *increase_num(void *num_ptr)
-{
-	int *x_ptr = (int *)num_ptr;
 
-	while( ++(*x_ptr) < 100 );
+
+// this function is run by the second thread
+void * increase_num(void *num_ptr)
+{
+	int * x_ptr = (int *) num_ptr;
+
+	while( *x_ptr < 100 ){
+		(*x_ptr)++;
+	}
+
 	printf("x increament finished\n");
+
+	return 0;
 }
 
 int main()
 {
+	int ret;
+	pthread_t tid;
 	int x = 0;
 	int y = 0;
-	pthread_t new_thread;
-	int ret_pthread_create, ret_pthread_join;
 
-	printf("origin:\nx: %d, y: %d\n", x, y);
+	printf("x: %d, y: %d\n", x, y);
 
-
-	// int pthread_create(pthread_t *thread,
-	// 			const pthread_attr_t *attr,
-	// 			void *(*start_routine) (void *),
-	// 			void *arg);
-	//
-	// On success, pthread_create() returns 0;
-
-	ret_pthread_create = pthread_create(&new_thread, NULL, increase_num, &x);
-	if ( ret_pthread_create != 0){
+	ret = pthread_create(&tid, NULL, increase_num, &x);
+	if ( ret != 0){
 		perror("pthread_create");
 	}
 
 
-	while(++y < 100);
-	printf("y increament  finished\n");
+	while(y < 100){
+		y++;
+	}
+	printf("y increament finished\n");
 
 
-	// pthread_join - join with a terminated thread
-	// int pthread_join(pthread_t thread, void **retval);
-	//
-	// On success, pthread_join() returns 0
-	ret_pthread_join = pthread_join(new_thread, NULL);
-	if( ret_pthread_join != 0){
+	ret = pthread_join(tid, NULL);
+	if( ret != 0){
 		perror("pthread_join");
 	}
 
-	printf("final:\nx: %d, y: %d\n", x, y);
+	printf("final x: %d, y: %d\n", x, y);
 
 	return 0;
 }
