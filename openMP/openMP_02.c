d4ac406e479a2d340b8c6da89aa0556c3556703e
Lin Emsber <linemsber@gmail.com>
2017-08-08 00:43:22 +0800

    OpenMP: synchronization, critical and atomic

--- openMP/openMP_02.c
+++ openMP/openMP_02.c
@@ -1,39 +1,30 @@
-/* for loop of openMP. */
+/* For loop of openMP. */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
-#define CHUNKSIZE 10
-#define N 100
+#define CHUNK_SIZE 10
+#define N 2001
 
 int main (int argc, char *argv[])
 {
-	int nthreads, tid, i, chunk;
+	int i;
 	float a[N], b[N], c[N];
 
 	/* Initializations */
-	for (i = 0; i < N; i++)
-		a[i] = b[i] = i * 1.0;
+	for (i = 0; i < N; i++){
+		a[i] = i * 1.0;
+		b[i] = a[i] * 10 + i * 1.0;
+	}
 
-	chunk = CHUNKSIZE;
-
-	#pragma omp parallel shared(a,b,c,nthreads,chunk) private(i,tid)
+	#pragma omp parallel shared(a, b, c) private(i)
 	{
-		tid = omp_get_thread_num();
-		if (tid == 0) {
-			nthreads = omp_get_num_threads();
-			printf("Number of threads = %d\n", nthreads);
-		}
-		printf("Thread %d starting...\n",tid);
-
-		#pragma omp for schedule(dynamic,chunk)
 		for (i = 0; i < N; i++) {
-			c[i] = a[i] + b[i];
-			printf("Thread %d: c[%d]= %f\n",tid,i,c[i]);
+				c[i] = a[i] + b[i];
 		}
-
-	}  /* end of parallel section */
+	}
+	printf("c[%d] = %f\n", N - 1, c[N-1]);
 
 	return 0;
 }
