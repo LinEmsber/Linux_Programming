07b024cc2554cb1659f1aef9134bfff7413c2881
Lin Emsber <linemsber@gmail.com>
2017-08-08 00:43:22 +0800

    Synchronization, critical and atomic

---
+++ openMP/openMP_03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main()
+{
+	int i;
+	int r = 0, c = 0, a = 0;
+	int iter = 5000000;
+
+	printf("OpenMP threads = %d\n", omp_get_max_threads() );
+
+	#pragma omp parallel reduction(+:r) shared(c, a)
+	{
+		#pragma omp for
+		for ( i = 0; i < iter; i++ ) {
+			r++;
+			#pragma omp critical
+			c++;
+			#pragma omp atomic
+			a++;
+		}
+	}
+	printf("reduce   = %d\n"
+		"critical = %d\n"
+		"atomic   = %d\n", r, c, a);
+
+	return 0;
+}
